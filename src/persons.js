846a2b869885142efe8e0cb93dbb9c670015b613
Leonardo <leo.braga@eaportal.org>
2025-10-23 12:08:09 -0300

    Colocando array na pasta persons.js

---
+++ src/persons.js
@@ -0,0 +1,46 @@
+const persons = [
+
+    {
+
+     name:"Leonardo Braga de Souza",
+     email: "bragaleonardo946@gmail.com",
+     age: 10,
+     nickname: "Leozinho"
+
+
+
+    },
+    
+    {
+
+     name:"Kamilla Ribeiro",
+     email: "ribeirinhakamilla@gmail.com",
+     age: 30,
+     nickname: "kamilla123"
+
+
+
+    },
+    
+    {
+
+     name:"Gabriel Bonfim",
+     email: "gayv4468@gmail.com",
+     age: 10,
+     nickname: "maufim"
+
+
+
+    },
+   
+    {
+
+     name:"Gabriel Scuoteguazzza Portilho",
+     email: "representantemagnaninosoberanosupremo@gmail.com",
+     age: 70,
+     nickname: "gaybriel"
+
+
+
+    },
+]
