3358423803a4bd143600b63b6b9af9709c99e644
Leonardo <leo.braga@eaportal.org>
2025-11-06 08:35:54 -0300

    Colocando o request.body e o app.use

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const app = express()
 const port = 3333
 
 app.use(cors())
+app.use(express.json())
 
 // GET, POST, DELETE, PATCH, PUT
 
@@ -16,7 +17,7 @@ app.get("/", (request, response) => {
 
 app.post("/cadastrar", (request,response) => {
 
-    console.log(request)
+    console.log(request.body)
 
 
 } )
